6da41cb1c97157f8e44b7ba1ef264cd3c8d4fe7e
Fernando Paniagua <fpaniagua.unity.2018.9@gmail.com>
2019-03-04 14:05:04 +0100

    Initial commit

---
+++ Assets/_GameObjects/Scripts/Douglas.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Douglas : MonoBehaviour {
+    public Transform[] puntosPatrulla;
+    private NavMeshAgent agente;
+    private Animator animador;
+    private const string ANIM_ISWALKING = "isWalking";
+    private int indiceNavegacion = 0;
+    private enum Estado { Idle, Walking };
+    private Estado estado = Estado.Idle;
+    private void Awake()
+    {
+        agente = GetComponent<NavMeshAgent>();
+        animador = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        AsignarDestinoReal(indiceNavegacion);
+    }
+
+    private void AsignarDestinoReal(int indice)
+    {
+        estado = Estado.Walking;
+        agente.destination = puntosPatrulla[indice].position;
+        animador.SetBool(ANIM_ISWALKING, true);
+    }
+
+    private void Update()
+    {
+        if (estado!=Estado.Idle && agente.remainingDistance < 0.1f)
+        {
+            estado = Estado.Idle;
+            animador.SetBool(ANIM_ISWALKING, false);
+            Invoke("AsignarDestino", 2);
+        }
+    }
+
+    private void AsignarDestino()
+    {
+        //Navegacion 'aleatoria' con control de errores
+        int nuevoIndice = Random.Range(0, puntosPatrulla.Length);
+        if (nuevoIndice == indiceNavegacion)
+        {
+            nuevoIndice++;
+        }
+        if (nuevoIndice == puntosPatrulla.Length)
+        {
+            nuevoIndice = 0;
+        }
+        indiceNavegacion = nuevoIndice;
+
+        /*
+        //Navegacion aleatoria pura
+        int nuevoIndice;
+        do
+        {
+            nuevoIndice = Random.Range(0, puntosPatrulla.Length);
+        } while (nuevoIndice == indiceNavegacion);
+        indiceNavegacion = nuevoIndice;
+        */
+
+        /*
+        //Navegacion secuencial
+        indiceNavegacion++;
+        if (indiceNavegacion == puntosPatrulla.Length)
+        {
+            indiceNavegacion = 0;
+        }
+        */
+        AsignarDestinoReal(indiceNavegacion);
+    }
+}
---
+++ Assets/_GameObjects/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private const string ANIM_ISWALKING = "isWalking";
+    private float y;
+    private float x;
+    private Animator animador;
+    private void Awake()
+    {
+        animador = GetComponent<Animator>();
+    }
+    void Update () {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+        if (y > 0.1f)
+        {
+            Avanzar();
+        } else
+        {
+            Parar();
+        }
+        if (x != 0)
+        {
+            Rotar();
+        }
+	}
+    private void Avanzar()
+    {
+        animador.SetBool(ANIM_ISWALKING, true);
+    }
+    private void Parar()
+    {
+        animador.SetBool(ANIM_ISWALKING, false);
+    }
+    private void Rotar()
+    {
+        transform.Rotate(0, x, 0);
+    }
+}
